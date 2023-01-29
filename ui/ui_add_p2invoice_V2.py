c4a11da23f01a97700c303695e4e0a1e8fbe1d1f
Khang <55547021+Achicken7301@users.noreply.github.com>
2023-01-29 16:52:49 +0700

    update

--- ui/ui_add_p2invoice_V2.py
+++ ui/ui_add_p2invoice_V2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'd:\banhb\Documents\Projects\2023\NamKhangStore\ui\add_p2invoice_V2.ui'
+# Form implementation generated from reading ui file 'd:\banhb\Documents\Projects\2023\simple-store\ui\add_p2invoice_V2.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.7
 #
--- ui/ui_main.py
+++ ui/ui_main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'd:\banhb\Documents\Projects\2023\NamKhangStore\ui\main.ui'
+# Form implementation generated from reading ui file 'd:\banhb\Documents\Projects\2023\simple-store\ui\main.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.7
 #
